2fa76e6fe9b9b8df67ca3fe359e654ac4af716e5
Christos Aniftos <aniftos@google.com>
2023-11-01 11:59:10 +0000

    about us page added

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/about-us")
+async def root():
+    return {"message": "About us: We are a company that sells products"}
